ba327fcfc6823f446b78e77950621a7ab37d970b
Yogesh <sarodeyogesh19@gmail.com>
2026-04-14 16:36:09 +0530

    Added Words Duplicate Logic

--- src/Testing/DuplicateChars.java
+++ src/Testing/DuplicateChars.java
@@ -5,6 +5,7 @@ public class DuplicateChars {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Duplicate Characters in String
 		String s="aabcddtrgtg"; //adtg
 
 		String result="";
@@ -22,6 +23,29 @@ public class DuplicateChars {
 			}
 		}
 		System.out.println("Duplicate Characters in String : " +result);
+		
+		//Duplicate Words in String
+		
+		String s1="Java Selenium API Java Python";
+		
+		String words[]=s1.split(" ");
+		
+		for(int i=0;i<words.length;i++)
+		{
+			int count=1;
+			
+			for(int j=i+1;j<words.length;j++)
+			{
+				if(words[i].equals(words[j]))
+				{
+					count++;
+				}
+			}
+			if(count>1)
+			{
+				System.out.println("Duplicate Words in String : " +words[i]);
+			}
+		}
 	}
 
 }
